44a795cd28fef50eeed3fa733eb6c7af528de97c
iCalculated <fufa0001@gmail.com>
2019-08-25 09:25:23 -0500

    final cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,8 @@
 extern crate rand;
 use rand::Rng;
 use rand::distributions::{Distribution, Uniform};
-
 use std::env;
 use image::{ImageBuffer, Pixel, Rgb};
-
-// if anyone asks you didn't see this
-pub fn random() -> (u8, u8, u8) {
-    unsafe {
-        static mut STATE: u64 = 0x123456789abcdef0;
-        STATE = STATE.wrapping_mul(2862933555777941757)
-            .wrapping_add(3037000493);
-        (((STATE >> 56) & 0xff) as u8,
-        ((STATE >> 48) & 0xff) as u8,
-        ((STATE >> 40) & 0xff) as u8)
-    }
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -37,6 +24,7 @@ fn main() {
     let mut rng = rand::thread_rng();
     let uniform = Uniform::from(1..255);
     let mut img = ImageBuffer::new(width,height);
+    let mut comp = ImageBuffer::new(width,height);
     for x in 0..width {
         for y in 0..height {
             let (r,g,b): (u8,u8,u8) = rng.gen::<(u8, u8, u8)>();
